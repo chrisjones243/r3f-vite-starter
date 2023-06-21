bc43f9fd335d1b1db10a1ab0b545979df853d196
chrisjones243 <38958680+chrisjones243@users.noreply.github.com>
2023-06-21 18:29:40 +0100

    Some animations

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+
+  const { animation } = useControls({
+    animation: {
+      value: "Sitting Pose",
+      options: ["Sitting Pose", "Thriller", "Wave"],
+    },
+  });
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      <group position={[0, -0.5, 0]}>
+        <Avatar animation={animation} />
+      </group>
+      <ambientLight intensity={1} />
     </>
   );
 };
